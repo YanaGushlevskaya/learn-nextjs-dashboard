eb4f8d268d478eeabe5abe4628f0c020fc9d29c3
Yana Hushlevska <jasynja@ukr.net>
2026-06-10 14:58:31 +0300

    chapter 10: adding search and pagination

---
+++ app/hooks/useDebouncedCallback.tsx
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+export function useDebouncedCallback<T extends (...args: any[]) => void>(
+  callback: T,
+  delay = 500,
+) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const debouncedCallback = useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        callbackRef.current(...args);
+      }, delay);
+    },
+    [delay],
+  );
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return debouncedCallback;
+}
